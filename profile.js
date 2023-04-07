014eef263bb5454d004d058d9e26b0b74b737d59
philipslozin@gmail.com <pslozin@gmail.com>
2023-04-07 14:11:20 -0400

    V2

--- profile.js
+++ profile.js
@@ -1,3 +1,20 @@
+//V2
+const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+const random = Math.floor(Math.random() * months.length);
+console.log(random, months[random]);
+
+
+function randomRest()
+{
+alert(random)
+}
+
+let rest = document.querySelector(".restsug")
+
+rest.addEventListener('click', randomRest)
+
+
 
 function showSkiing()
 {
